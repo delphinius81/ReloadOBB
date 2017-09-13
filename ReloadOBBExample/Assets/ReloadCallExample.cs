22affe8c254411b764f3e3513ab2e77ba9e09760
Dustin Chertoff <dustin@fearlessvr.com>
2017-09-13 13:00:44 -0700

    Example of calling the ReloadOBB activity from within Unity

---
+++ ReloadOBBExample/Assets/ReloadCallExample.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.VR;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ReleadOBB {
+   public class ReloadCallExample : MonoBehaviour {
+      void Start() {
+			// This calls the reload obb activity. You should call this from an applicable point in your code.
+			// For example, the first time your application runs, or after retrieving the OBB file from GooglePlay.
+			// It should only be called when running on device.
+			#if UNITY_ANDROID && !UNITY_EDITOR
+			CallReloadOBB.Reload();
+			#endif
+      }
+   }
+}
---
+++ ReloadOBBExample/Assets/ReloadOBB/CallReloadOBB.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallReloadOBB {
+   public static void Reload()
+   {
+      using (AndroidJavaClass unity_player = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+      {
+         AndroidJavaObject current_activity = unity_player.GetStatic<AndroidJavaObject>("currentActivity");
+         Debug.Log("Attempting to launch external reload activity.");
+         var jc = new AndroidJavaClass("com.delphinius81.reloadobb.ReloadObbActivity");
+         //jc.CallStatic("Launch", current_activity);
+         jc.Call("Launch", current_activity);
+      }
+   }
+}
